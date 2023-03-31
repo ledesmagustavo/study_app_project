bcb694b0701f16062c65225e8f107b868da3107a
ledesmagustavo <g.ledesma0114@gmail.com>
2023-03-31 19:20:30 -0400

    yes

---
+++ src/controller/themesPropertiesOLD/themesProperties.controller.js
@@ -0,0 +1,113 @@
+const { sequelize } = require("../../connection");
+const { ThemesPropertiesModel } = require("../../model/themesProperties.model");
+const ThemesPropertiesService = require('../../Service/themesPropertiesOld.service');
+const listar = async function (req, res) {
+    console.log("listar themes prioperties controller");
+
+    try {
+        
+       const themes_properties = await ThemesPropertiesService.listar(req.query.filtro || '');
+
+        if (themes_properties) {
+            // En users[0] se encuentra el listado de lo que se recupera desde el SQL
+            res.json({
+                success: true,
+                propiedadesTemas: themes_properties
+            });
+        } else {
+            res.json({
+                success: true,
+               propiedadesTemas: []
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            error: error.message
+        });
+    }
+};
+
+const consultarPorCodigo = async function (req, res) {
+    console.log("consultar propiedades de temas");
+
+    try {
+        // Buscar en la base de datos por codigo
+        const themesPropertiesModelResult = await ThemesPropertiesService.consultarPorCodigo(req.query.filtro || '');
+
+        if (themesPropertiesModelResult) {
+            res.json({
+                success: true,
+                propiedadesTemas: themesPropertiesModelResult
+            });
+        } else {
+            res.json({
+                success: true,
+                propiedadesTemas: null
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            error: error.message
+        });
+    }
+};
+
+const actualizar = async function (req, res) {
+    console.log("actualizar propiedades de temas");
+
+
+    // res.send("actualiza los usuarios")
+    // variables
+    let usuarioRetorno = null; 
+
+    try {
+        
+    usuarioRetorno = await ThemesPropertiesService.actualizar(
+        req.body.id, 
+        req.body.property_name, 
+        req.body.property_value);
+
+        res.json({
+            success:true,
+            user: usuarioRetorno
+        })
+
+    } catch (error) {
+        console.log(error);
+
+         res.json({
+        success: false,
+        error: error.message
+    });
+};
+}
+
+
+const eliminar = async function (req, res) {
+    console.log("eliminar propiedades de temas");
+
+    // borrado fisico
+   //  UserModel.destroy(req.params.id);
+    try {
+
+        await sequelize.query("UPDATE themes_properties SET deleted = true WHERE id=  " + req.params.id);
+        res.json({
+            success: true
+        });
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            error : error.message
+        });
+    }
+};
+
+
+module.exports = {
+    listar, busquedaPorCodigo: consultarPorCodigo, actualizar, eliminar
+};
